f643d928820865b78860970319fd76a72d1dc1e8
dev-Jong <152872267+dev-Jong@users.noreply.github.com>
2023-12-20 10:55:19 +0900

    img preLoad

--- assets/js/block.js
+++ assets/js/block.js
@@ -1,4 +1,40 @@
 $(window).on('load', function(){
+  const preLoadImgSrc = [
+    "../assets/images/block0.svg",
+    "../assets/images/block1.svg",
+    "../assets/images/block2.svg",
+    "../assets/images/block3.svg",
+    "../assets/images/block4.svg",
+    "../assets/images/block5.svg",
+    "../assets/images/block6.svg",
+    "../assets/images/block7.svg",
+    "../assets/images/block8.svg",
+    "../assets/images/block9.svg",
+    "../assets/images/block10.svg",
+    "../assets/images/block11.svg",
+    "../assets/images/block12.svg",
+    "../assets/images/block13.svg",
+    "../assets/images/block14.svg",
+    "../assets/images/block15.svg",
+    "../assets/images/block16.svg",
+    "../assets/images/block17.svg",
+    "../assets/images/block18.svg",
+    "../assets/images/block19.svg",
+    "../assets/images/block20.svg",
+    "../assets/images/block21.svg",
+    "../assets/images/block22.svg",
+    "../assets/images/block23.svg",
+  ];
+
+  const preLoadImg = (images) => {
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+    });
+  };
+
+  preLoadImg(preLoadImgSrc);
+
   const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
   let width = 1400;
@@ -39,16 +75,18 @@ $(window).on('load', function(){
   
   // Random Shapes
   
-  for (let i = 0; i < 24; i++) {
-    if (i < 14) {
-      World.add(
-        world, 
-        Bodies.rectangle(Math.random() * width, -Math.random() * height, rectWidth, rectWidth, {isStatic: false, angle:Math.random()*i, render: { sprite: { texture: '../assets/images/block'+i+'.svg'}}}));
-    } else {
-      World.add(
-        world,
-        Bodies.circle(Math.random() * width, -Math.random() * height, circleWidth, {isStatic: false, angle:Math.random()*i, render: { sprite: { texture: '../assets/images/block'+i+'.svg'}}})
-      );
+  // image.onload = function(){
+    for (let i = 0; i < 24; i++) {
+      if (i < 14) {
+        World.add(
+          world, 
+          Bodies.rectangle(Math.random() * width, -Math.random() * height, rectWidth, rectWidth, {isStatic: false, angle:Math.random()*i, render: { sprite: { texture: '../assets/images/block'+i+'.svg'}}}));
+      } else {
+        World.add(
+          world,
+          Bodies.circle(Math.random() * width, -Math.random() * height, circleWidth, {isStatic: false, angle:Math.random()*i, render: { sprite: { texture: '../assets/images/block'+i+'.svg'}}})
+        );
+      }
     }
-  }
+  // }
 });
